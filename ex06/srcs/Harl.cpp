266779bc185ead1636aa9f9997a9f084d4dfbe0d
Clement Salaun <csalaun@f1br3s5.clusters.42paris.fr>
2025-12-12 15:52:48 +0100

    modif on formatage

--- ex06/srcs/Harl.cpp
+++ ex06/srcs/Harl.cpp
@@ -4,23 +4,27 @@ Harl::Harl() {};
 Harl::~Harl() {};
 
 void Harl::debug(void) {
-    std::cout << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
     << std::endl;
 }
 
 void Harl::info(void) {
+    std::cout << "[ INFO ]" << std::endl;
     std::cout <<
-        "INFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+        "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
     << std::endl;
 }
 
 void Harl::warning(void) {
-    std::cout << "WARNING: I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
     << std::endl;
 }
 
 void Harl::error(void) {
-    std::cout << "ERROR: This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 const std::string Harl::_name[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
